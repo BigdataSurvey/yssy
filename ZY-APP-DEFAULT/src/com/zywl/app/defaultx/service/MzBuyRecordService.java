7aa0d65c4e3e40e037fe9a4a5a0bd24b898a7661
Ns <847502314@qq.com>
2025-07-11 13:34:25 +0800

    慢涨模块开发中

--- ZY-APP-DEFAULT/src/com/zywl/app/defaultx/service/MzBuyRecordService.java
+++ ZY-APP-DEFAULT/src/com/zywl/app/defaultx/service/MzBuyRecordService.java
@@ -1,6 +1,5 @@
 package com.zywl.app.defaultx.service;
 
-import com.zywl.app.base.bean.MzBuyOrder;
 import com.zywl.app.base.bean.MzBuyRecord;
 import com.zywl.app.defaultx.dbutil.DaoService;
 import org.springframework.stereotype.Service;
